455de20e8c3ad3874e38fc95cbb7514757c7ae89
lasergangers <celloadrian29@protonmail.com>
2021-08-24 22:06:46 +0700

    fix: fix screen flicker when transitioning to another page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, ActivityIndicator } from "react-native";
+import { View, ActivityIndicator, useColorScheme, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Register from "./src/pages/Register";
 import WifiSetup from "./src/pages/WifiSetup";
@@ -20,6 +20,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [isFirstTime, setIsFirstTime] = useState(false);
+  const colorScheme = useColorScheme();
+
+  const themeContainerStyle = colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
 
   const checkIfFirstTime = async () => {
     try {
@@ -40,26 +43,40 @@ export default function App() {
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        {loading ? (
-          <Stack.Screen name="Loading" component={Loading}/>
-        ) : isFirstTime ? (
-          <>
-            <Stack.Screen name="Homescreen" component={Homescreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="WifiSetup" component={WifiSetup} />
-            <Stack.Screen name="Homescreen" component={Homescreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={[styles.Container, themeContainerStyle]}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          {loading ? (
+            <Stack.Screen name="Loading" component={Loading} />
+          ) : isFirstTime ? (
+            <>
+              <Stack.Screen name="Homescreen" component={Homescreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Register" component={Register} />
+              <Stack.Screen name="WifiSetup" component={WifiSetup} />
+              <Stack.Screen name="Homescreen" component={Homescreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+  },
+  lightContainer: {
+    backgroundColor: "#d0d0c0",
+  },
+  darkContainer: {
+    backgroundColor: "#191720",
+  },
+})
