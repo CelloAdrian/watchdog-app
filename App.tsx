fd0280cc721e8644995d9d7c10c1862877d21a68
lasergangers <celloadrian29@protonmail.com>
2021-08-24 15:45:13 +0700

    feat: show register screen only when user hasnt registered yet

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, ActivityIndicator } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Register from "./src/pages/Register";
 import WifiSetup from "./src/pages/WifiSetup";
 import Homescreen from "./src/pages/Homescreen";
 
+const Loading = () => {
+  return (
+    <View>
+      <ActivityIndicator />
+    </View>
+  );
+};
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [isFirstTime, setIsFirstTime] = useState(false);
+
+  const checkIfFirstTime = async () => {
+    try {
+      const value = await AsyncStorage.getItem("username");
+
+      if (value !== null) {
+        setIsFirstTime(true);
+      }
+    } catch (err) {
+      console.log("Error @checkIfFirstTime: ", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkIfFirstTime();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -15,9 +46,19 @@ export default function App() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="WifiSetup" component={WifiSetup} />
-        <Stack.Screen name="Homescreen" component={Homescreen} />
+        {loading ? (
+          <Stack.Screen name="Loading" component={Loading}/>
+        ) : isFirstTime ? (
+          <>
+            <Stack.Screen name="Homescreen" component={Homescreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="WifiSetup" component={WifiSetup} />
+            <Stack.Screen name="Homescreen" component={Homescreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
